9094a719f1a00029797eac801474d2e22f7e878e
SiriVennela24-star <svennela490@gmail.com>
2025-07-26 17:16:23 +0530

    Update vennela_py.py

--- vennela_py.py
+++ vennela_py.py
@@ -10,7 +10,6 @@ Original file is located at
 
 # Import necessary libraries
 import streamlit as st
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
